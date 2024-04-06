cdbeb826f8471ccddaed80bf33ec2ef79dfe6120
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-04-05 21:02:27 -0700

    script.js pseudocode

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 // script.js BreakBound 
 
+/*
+
+// VARIABLES 
+
+workTime    // time spent working 
+restTime    // time spent on eye rest 
+pauseTime   // time spent paused 
+breakTime   // time spent on break 
+
+isTimerRunning  // bool to check if timer is active 
+isWorking       // bool to track work time 
+isResting       // bool to track rest time 
+isOnBreak       // bool to track break time 
+isPaused        // bool to track pause time 
+
+// FUNCTIONS 
+
+minutesToSeconds
+startTimer 
+pauseTimer 
+resumeTimer 
+resetTimer 
+updateTimer 
+formatTime 
+
+*/
