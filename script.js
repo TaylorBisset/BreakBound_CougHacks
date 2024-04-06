ea39f947ea7e1fb47a66b707d3f4c610e310717d
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-04-05 22:04:36 -0700

    add formatTime()

--- script.js
+++ script.js
@@ -17,8 +17,23 @@ let totalRestTime = 0;
 let totalBreakTime = 0; 
 let totalPauseTime = 0; 
 
-/*
+
 // FUNCTIONS 
+
+function formatTime(seconds) {
+    let hours = Math.floor(seconds / 3600); 
+    let minutes = Math.floor((seconds % 3600) / 60); 
+    let remainingSeconds = seconds % 60; 
+
+                    // add a leading zero if single digit
+    let formattedHours = hours < 10 ? `0${hours}` : hours; 
+    let formattedMinutes = minutes < 10 ? `0${minutes}` : minutes; 
+    let formattedSeconds = remainingSeconds < 10 ? `0${remainingSeconds}` : remainingSeconds; 
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`; 
+}
+
+/*
 
 minutesToSeconds
 startTimer 
